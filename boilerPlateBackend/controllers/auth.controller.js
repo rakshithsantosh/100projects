6382f5a514274674c5869ce184ca69c85cfa7d37
rakshith_s <rakshithsantosh@gmail.com>
2025-03-13 10:23:21 +0530

    authRoutes

--- boilerPlateBackend/controllers/auth.controller.js
+++ boilerPlateBackend/controllers/auth.controller.js
@@ -9,25 +9,28 @@ export const signup = async (req, res, next) => {
   const newUser = new user({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(200).json(message:"new user created")
+    res.status(200).json({ message: "new user created" });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
-export const signin = async (req,res,next)=>{
-    const {email,password}= req.body;
-    try {
-        const validUser = await user.findOne({email})
-        if(!validUser) return next(errorHandler(404,'User not Found'))
-        const validPassword = bcryptjs.compareSync(password,validUser.password)
-        if(!validPassword) return next(errorHandler(403,'Wrong Credentials'))
-        //add a token to the cookie of the browser
-        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
-        const {password:hashedPassword,...rest} = validUser._doc
-        const expiryDate = new Date(Date.now()+3600000); //1 hour
-        res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(rest)
-    } catch (error) {
-        next(error)
-    }
-}
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await user.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not Found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(403, "Wrong Credentials"));
+    //add a token to the cookie of the browser
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); //1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- boilerPlateBackend/index.js
+++ boilerPlateBackend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectToDb } from "./db/db.js";
 
