fe304b967ca10efcf022e9eaa58bb4ff04d50e2f
rakshith_s <rakshithsantosh@gmail.com>
2025-03-17 21:50:37 +0530

    handling files in nodejs

---
+++ todo/todo.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+const loadTask = () => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveTasks = (tasks) => {
+  const dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const addTask = (task) => {
+  const tasks = loadTask();
+  tasks.push({ task });
+  saveTasks(tasks);
+  console.log("task added", task);
+};
+
+const listTasks = () => {
+  const tasks = loadTask();
+  tasks.forEach((task, index) => console.log(`${index + 1} task`));
+};
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+if (command === "add") {
+  addTask(argument);
+} else if (command === "list") {
+  listTasks();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("command not found");
+}
