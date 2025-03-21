5b57ed5eac44bbdb46b7e1b0890e5a14fb784551
rakshith_s <rakshithsantosh@gmail.com>
2025-03-21 12:47:14 +0530

    logging with winston and morgan

--- express/index.js
+++ express/index.js
@@ -1,10 +1,32 @@
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
 //setting to accept json data from frontend
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+//middleware for loggers
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
